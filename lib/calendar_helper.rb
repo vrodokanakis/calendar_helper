634dbf14ae354d26afd19c470613be7333d2d117
Konstantinos Christainas <konstantinos.christainas@generation-y.gr>
2014-10-16 12:35:32 +0300

    Add scope to th of table

--- lib/calendar_helper.rb
+++ lib/calendar_helper.rb
@@ -126,12 +126,12 @@ module CalendarHelper
     if (options[:month_header])
       cal << %(<tr>)
       if options[:previous_month_text] or options[:next_month_text]
-        cal << %(<th colspan="2">#{options[:previous_month_text]}</th>)
+        cal << %(<th colspan="2" scope="col">#{options[:previous_month_text]}</th>)
         colspan = 3
       else
         colspan = 7
       end
-      cal << %(<th colspan="#{colspan}" class="#{options[:month_name_class]}"><div class="container">)
+      cal << %(<th colspan="#{colspan}" class="#{options[:month_name_class]}" scope="col"><div class="container">)
 
       cal << %(<div class="previous_month">#{options[:previous_month_link]}</div>) if options[:previous_month_link]
       if (options[:year_header])
@@ -142,7 +142,7 @@ module CalendarHelper
       cal << %(<div class="next_month">#{options[:next_month_link]}</div>) if options[:next_month_link]
       cal << %(</div></th>)
 
-      cal << %(<th colspan="2">#{options[:next_month_text]}</th>) if options[:next_month_text]
+      cal << %(<th colspan="2" scope="col">#{options[:next_month_text]}</th>) if options[:next_month_text]
       cal << %(</tr>)
     end
 
